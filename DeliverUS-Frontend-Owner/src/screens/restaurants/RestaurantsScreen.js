683b25e4f389f44b1f70cb1983fbc50d611cc508
German220203 <gerojegar@alum.us.es>
2024-04-23 18:20:33 +0200

    Commit

--- DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantsScreen.js
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantsScreen.js
@@ -6,26 +6,49 @@ import { getAll } from '../../api/RestaurantEndpoints'
 import * as GlobalStyles from '../../styles/GlobalStyles'
 
 export default function RestaurantsScreen({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <TextRegular style={{ fontSize: 16, alignSelf: 'center', margin: 20 }}>Random Restaurant</TextRegular>
+  // Component state, que almacenara en un array la lista de restaurantes
+  // restaurants = array que almacenara la lista de restaurantes, setRestaurants = funcion que seteara restaurants con la lista
+  // dada como parametro a dicha funcion
+  const [restaurants, setRestaurants] = useState([])
+
+  // Effect que tomara dos paramentros
+  // 1- Funcion a ejecutar, en este caso esperar dos segundos y llamar a setRestaurants para añadir
+  // setear restaurants con el metodo getAll
+  // 2- Parametro opcional que identifica que objetos haran que, al ser cambiados, se ejecute la funcion del primer
+  // parametro. Si se deja vacia, se ejecuta la funcion cada vez que se renderice la pagina.
+  useEffect(() => {
+    console.log('Loading restaurants, please wait 2 seconds')
+    setTimeout(() => {
+      setRestaurants(getAll)
+      console.log('Restaurants loaded')
+    }, 2000)
+  }, [])
+
+  // Funcion que se encarga de mostar / renderizar un objeto que contiene todas sus propiedades
+  // En este caso, se muestra por cada restaurante, un boton con el nombre del restaurante que,
+  // al pulsarlo, redirige a la vista de los detalles de ese restaurante
+  const renderRestaurant = ({ restaurant }) => {
+    return (
       <Pressable
-        onPress={() => {
-          navigation.navigate('RestaurantDetailScreen', { id: Math.floor(Math.random() * 100) })
-        }}
-        style={({ pressed }) => [
-          {
-            backgroundColor: pressed
-              ? GlobalStyles.brandBlueTap
-              : GlobalStyles.brandBlue
-          },
-          styles.actionButton
-        ]}
-      >
-        <TextRegular textStyle={styles.text}>
-          Go to Random Restaurant Details
+      style = {styles.row}
+      onPress = {() => {
+        navigation.navigate('RestaurantDetaileScreen', { id: restaurant.id })
+      }}>
+        <TextRegular>
+          {restaurant.name}
         </TextRegular>
       </Pressable>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        style={styles.container}
+        data={restaurants}
+        renderItem={renderRestaurant}
+        keyExtractor={item => item.id.toString()}
+      />
     </View>
   )
 }
